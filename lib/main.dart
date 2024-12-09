4fb52f7a43d1895ebc21ad424ec49c832fff472f
arthishaxom <ashishpothal@gmail.com>
2024-12-09 09:00:57 +0530

    testing keys

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive_ce_flutter/hive_flutter.dart';
 import 'package:kiittime/models/theme_provider.dart';
@@ -20,9 +21,11 @@ void main() async {
   await Hive.initFlutter();
   await Hive.openBox('timetable');
 
+  await dotenv.load(fileName: "env");
+
   await Supabase.initialize(
-    url: const String.fromEnvironment('URL'),
-    anonKey: const String.fromEnvironment('ANON'),
+    url: dotenv.env['URL']!,
+    anonKey: dotenv.env['ANON']!,
   );
 
   runApp(
